2f8a9e7d37b797393b4f732cc97400e17de9f7d0
Work <Work@MSI>
2023-09-26 12:22:39 -0500

    Ended on a problem with omitting braces

--- sept26-early/Source.cpp
+++ sept26-early/Source.cpp
@@ -1,14 +1,27 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
-    
-    //VERSION CONTROL!
-    cout << "Some changes!" << endl; 
-    //adsfasdf
 
-    //    asdfa
-    return 0;
+	string correctPassword = "123456";
+
+	cout << "Enter your password: " << endl; 
+	string enteredPassword; 
+
+	//getline()
+	cin >> enteredPassword; 
+
+	if (enteredPassword == correctPassword)
+
+		cout << "You're in! Welcome to your fabulous app!" << endl; 
+		cout << "Another statement!" << endl;
+
+	else
+		cout << "WRONG!" << endl;
+
+
 }
