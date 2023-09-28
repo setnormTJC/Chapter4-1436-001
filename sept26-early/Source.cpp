497acbe599b5632f919564b274b00b267631f48d
Work <Work@MSI>
2023-09-28 12:14:09 -0500

    Where we ended in morning on Sept28

--- sept26-early/Source.cpp
+++ sept26-early/Source.cpp
@@ -2,26 +2,90 @@
 #include <string>
 #include <fstream>
 
+#include<string>
+
 using namespace std;
 
 int main()
 {
+	//int speed = 62; 
 
-	string correctPassword = "123456";
+	//if (!(speed > 70))
+	//{
+	//	cout << "You ae NOT speeding " << endl; 
+	//}
+	// 
+	//cout enter a tweet
+	//	cin >> tweet
 
-	cout << "Enter your password: " << endl; 
-	string enteredPassword; 
+	//	//int 
+	//	ofstream output{ "someFileName.mp4" };
+	//	output << tweet << "and its length: " << tweet.length()
 
-	//getline()
-	cin >> enteredPassword; 
+	//double latitude; 
+	//double longitude; 
 
-	if (enteredPassword == correctPassword)
+	//cout << "Enter lat, then lon: " << endl;
+	//cin >> latitude >> longitude; 
 
-		cout << "You're in! Welcome to your fabulous app!" << endl; 
-		cout << "Another statement!" << endl;
+	//if (latitude == 35.07 and longitude == 135.23)
+	//{
+	//	cout << "You are more or less in Tamba-Sasayama" << endl;
+	//}
 
-	else
-		cout << "WRONG!" << endl;
+	//else if (latitude == -26.07 && longitude == 28.12)
+	//{
+	//	cout << "You are in Johanesburg" << endl;
+	//}
+
+	//else
+	//{
+	//	cout << "You are not in Tamba-Sasayama OR Johanesburg" << endl; 
+	//}
+
+	//string word = "true";
+	//int a = 1; //0 means false
+	////bool 
+
+	//int numericalGrade; 
+	//cout << "Enter a numerical grade: " << endl; 
+	//cin >> numericalGrade; 
+
+	//if (numericalGrade < 70 and numericalGrade > 0)
+	//{
+	//	cout << "D or worse!" << endl;
+	//}
+
+	//else if (numericalGrade > 70 and numericalGrade < 80  )
+	//{
+	//	cout << "The letter grade is a C." << endl; 
+	//}
+	//else if (numericalGrade >= 80 && numericalGrade < 90)
+	//{
+	//	cout << "B!" << endl;
+	//}
+
+	//else //catches everything not caught above
+	//{
+	//	cout << "A!" << endl;
+	//}
+
+
+	//string correctPassword = "123456";
+
+	//cout << "Enter your password: " << endl; 
+	//string enteredPassword; 
+
+	////getline()
+	//cin >> enteredPassword; 
+
+	//if (enteredPassword == correctPassword)
+
+	//	cout << "You're in! Welcome to your fabulous app!" << endl; 
+	//	cout << "Another statement!" << endl;
+
+	//else
+	//	cout << "WRONG!" << endl;
 
 
 }
