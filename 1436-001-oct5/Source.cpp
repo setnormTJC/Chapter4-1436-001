3dcdec791d264927c7cb60e7ecb3f50aa8d21cd1
setnormTJC <122695510+setnormTJC@users.noreply.github.com>
2023-10-05 10:59:52 -0500

    Revert "Update Source.cpp"

--- 1436-001-oct5/Source.cpp
+++ 1436-001-oct5/Source.cpp
@@ -1,1 +1,59 @@
+int main()
+{
 
+    //int break; 
+    //int fanSpeed = 0; 
+    char menuOption; 
+    cout << "Enter MENU option (f - for file - e (edit), g, p (that's all for now): " << endl; 
+
+    //cin >> fanSpeed;
+
+    cin >> menuOption; 
+
+    switch (menuOption)
+    {
+    case 'f':
+        cout << "FILE menu button selected ... displaying list of FILE options ..." << endl; 
+        //doFileThings(); 
+        //break; 
+
+    case 'e': 
+        cout << "EDIT" << endl;
+        break;
+
+    case 'g': 
+        cout << "GIT" << endl;
+        break; 
+
+    case 'p': 
+        cout << "Ypou selected PROJECT" << endl;
+        break; 
+
+    default: //analogous to "else" (catch all) 
+        cout << "You entered an UNKNOWN option!" << endl; 
+        break;
+    }
+
+    //if (fanSpeed == 0)
+    //{
+    //    cout << "Fan is OFF" << endl; 
+    //}
+
+    //else if (fanSpeed == 1)
+    //{
+    //    cout << "fan is running at a LOW speed";
+    //}
+
+    //else if (fanSpeed == 2)
+    //{
+    //    cout << "MEDIUM";
+    //}
+
+    //else //ASSUMES that user is "well-behaved" (enter only 1, 2, 3, or 0)
+    //{
+    //    cout << "HIGH speed";
+    //}
+
+
+    return 0;
+}
