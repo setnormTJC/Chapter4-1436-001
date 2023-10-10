3fb0a45109ac4500a6dae5c3907b54a8f0275ac4
Work <Work@MSI>
2023-10-10 09:20:02 -0500

    Mid lecture break on Oct 10 stop point

--- 1436-001-oct5/Source.cpp
+++ 1436-001-oct5/Source.cpp
@@ -1,58 +1,75 @@
+#include<iostream>
+
+using namespace std;
+
+//using std::cout; 
+
+
 int main()
 {
 
-    //int break; 
-    //int fanSpeed = 0; 
-    char menuOption; 
-    cout << "Enter MENU option (f - for file - e (edit), g, p (that's all for now): " << endl; 
+    ////int break; 
+    ////int fanSpeed = 0; 
+    //char menuOption; 
+    //cout << "Enter MENU option (f - for file - e (edit), g, p (that's all for now): " << endl; 
 
-    //cin >> fanSpeed;
+    ////cin >> fanSpeed;
 
-    cin >> menuOption; 
+    //cin >> menuOption; 
 
-    switch (menuOption)
+    //switch (menuOption)
+    //{
+    //case 'f':
+    //    cout << "FILE menu button selected ... displaying list of FILE options ..." << endl; 
+    //    //doFileThings(); 
+    //    //break; 
+
+    //case 'e': 
+    //    cout << "EDIT" << endl;
+    //    break;
+
+    //case 'g': 
+    //    cout << "GIT" << endl;
+    //    break; 
+
+    //case 'p': 
+    //    cout << "Ypou selected PROJECT" << endl;
+    //    break; 
+
+    //default: //analogous to "else" (catch all) 
+    //    cout << "You entered an UNKNOWN option!" << endl; 
+    //    break;
+    //}
+
+    ////if (fanSpeed == 0)
+    ////{
+    ////    cout << "Fan is OFF" << endl; 
+    ////}
+
+    ////else if (fanSpeed == 1)
+    ////{
+    ////    cout << "fan is running at a LOW speed";
+    ////}
+
+    ////else if (fanSpeed == 2)
+    ////{
+    ////    cout << "MEDIUM";
+    ////}
+
+    ////else //ASSUMES that user is "well-behaved" (enter only 1, 2, 3, or 0)
+    ////{
+    ////    cout << "HIGH speed";
+    ////}
+
+    int score = 85;
+
+    //cout << (80 <= score) << endl; //80 <= -185 -> FALSE (0)
+
+    if (80 <= score && score <= 89)
     {
-    case 'f':
-        cout << "FILE menu button selected ... displaying list of FILE options ..." << endl; 
-        //doFileThings(); 
-        //break; 
-
-    case 'e': 
-        cout << "EDIT" << endl;
-        break;
-
-    case 'g': 
-        cout << "GIT" << endl;
-        break; 
-
-    case 'p': 
-        cout << "Ypou selected PROJECT" << endl;
-        break; 
-
-    default: //analogous to "else" (catch all) 
-        cout << "You entered an UNKNOWN option!" << endl; 
-        break;
+        cout << "Letter grade is B." << endl;
     }
 
-    //if (fanSpeed == 0)
-    //{
-    //    cout << "Fan is OFF" << endl; 
-    //}
-
-    //else if (fanSpeed == 1)
-    //{
-    //    cout << "fan is running at a LOW speed";
-    //}
-
-    //else if (fanSpeed == 2)
-    //{
-    //    cout << "MEDIUM";
-    //}
-
-    //else //ASSUMES that user is "well-behaved" (enter only 1, 2, 3, or 0)
-    //{
-    //    cout << "HIGH speed";
-    //}
 
 
     return 0;
